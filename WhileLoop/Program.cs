f12db5ec78c6bf31a7ae35b467513b24bf047013
DimitarStankov <116435607+DimitarStankov@users.noreply.github.com>
2022-11-30 21:52:59 +0200

    Add project files.

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a command: ");
+            string command;
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+            Console.WriteLine(command);
+            Console.Write("Enter a command:");                
+            }
+        }
+    }
+}
